483e9b22f0eda6a8008e1f8ba24171e522d6f1a3
MelodyLC <dearonion@foxmail.com>
2017-01-19 10:41:50 -0800

    link js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -17,11 +17,22 @@ function initializePage() {
 	// example: $("#div-id").click(functionToCall);
 	$("a.thumbnail").click(projectClick);
 }
-
+/*
 function projectClick(e){
+	console.log("Project Click");
 	//prevent the page from reloading
 	e.preventDefault();
 	//In an event handler, $(this) refers to 
 	// the object that triggered the event
 	$(this).css("background-color", "#7fff00");
+}
+*/
+function projectClick(e){
+	// Cancel the default action, which prevents the page from reloading
+	e.preventDefault();
+
+	//In an event listener, $(this) is the element that fired the event
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
 }
